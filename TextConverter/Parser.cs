7c030f7386977afa94d6a61e6b326a4a1f24669a
Tezikov Roman <tez.romach@gmail.com>
2017-03-18 18:23:46 +0300

    implemented a parser

---
+++ TextConverter/Parser.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextConverter.ConverterBuilders;
+
+namespace TextConverter
+{
+    public class Parser
+    {
+        public string Parse(ConverterBuilder builder, string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return "";
+
+            foreach (string block in text.Split(new[] { "\n\r\n" }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                var innerList = block.Split('\n');
+                var keyWord = innerList[0].Split(' ')[0];
+
+                switch (keyWord)
+                {
+                    case KeyWords.Text:
+                        builder.AddText(block.Substring(keyWord.Length + 1));
+                        break;
+
+                    case KeyWords.Header1:
+                        builder.AddHeader(HeaderLevels.Level1, block.Substring(keyWord.Length + 1));
+                        break;
+                    case KeyWords.Header2:
+                        builder.AddHeader(HeaderLevels.Level2, block.Substring(keyWord.Length + 1));
+                        break;
+                    case KeyWords.Header3:
+                        builder.AddHeader(HeaderLevels.Level3, block.Substring(keyWord.Length + 1));
+                        break;
+
+                    case KeyWords.OrderedList:
+                        builder.AddOrderedList(innerList.Skip(1));
+                        break;
+                    case KeyWords.BulletedList:
+                        builder.AddBulletedList(innerList.Skip(1));
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+
+            return builder.ToString();
+        }
+
+    }
+    public static class KeyWords
+    {
+        public const string Text = "p";
+
+        public const string Header1 = "h1";
+        public const string Header2 = "h2";
+        public const string Header3 = "h3";
+
+        public const string OrderedList = "ordlist";
+        public const string BulletedList = "bullist";
+    }
+}
