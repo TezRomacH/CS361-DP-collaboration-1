e19f8cf2c913a7562f9bca5f0ddba64bfb01ab06
Tezikov Roman <tez.romach@gmail.com>
2017-03-19 14:20:02 +0300

    working on GUI, initialized dialogs

--- TextConverter/MainWindow.xaml.cs
+++ TextConverter/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using TextConverter.ConverterBuilders;
 
 namespace TextConverter
 {
@@ -21,24 +24,73 @@ namespace TextConverter
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ConverterBuilder builder;
+        private Parser.Parser parser;
+
+        private OpenFileDialog openFileDialog;
+        private SaveFileDialog saveFileDialog;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeDialogs();
+        }
+
+        private void InitializeDialogs()
+        {
+            openFileDialog = new OpenFileDialog
+            {
+                Filter = "All files (*.*)|*.*|Text files (*.txt)|*.txt"
+            };
+
+            saveFileDialog = new SaveFileDialog();
         }
 
         private void openMenuItem_OnClick(object sender, RoutedEventArgs e)
         {
-            
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    mainTextBox.Text = File.ReadAllText(openFileDialog.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(this,
+                        $"Text converter can't open a file!\n{ex.Message}",
+                        "Unexpected error!", MessageBoxButton.OK);
+                }
+            }
         }
 
         private void saveMenuItem_OnClick(object sender, RoutedEventArgs e)
         {
-            
+
         }
 
-        private void saveAsMenuItem_OnClick(object sender, RoutedEventArgs e)
+        private void parserButton_OnClick(object sender, RoutedEventArgs e)
         {
-            
+            var button = sender as Button;
+            string tag = button?.Tag.ToString();
+
+            // TODO: should we check button with other method?
+            if (tag == htmlParserButton?.Tag.ToString())
+            {
+
+            }
+            else if (tag == markdownParserButton?.Tag.ToString())
+            {
+
+            }
+        }
+
+        private void mainTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            // TODO: syntax highlighting
+            // ...
+
+            // Uncomment this when builder's methods are implemented
+            //resultTextBox.Text = parser.Parse(builder, (sender as TextBox)?.Text);
         }
     }
 }
