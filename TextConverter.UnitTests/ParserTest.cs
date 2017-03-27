9a99dadeb33097890b353b64e16a21a9e848f2ff
Tezikov Roman <tez.romach@gmail.com>
2017-03-27 11:36:31 +0300

    add new tests

---
+++ TextConverter.UnitTests/ParserTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TextConverter.Parser;
+
+namespace TextConverter.UnitTests
+{
+    [TestFixture]
+    public class ParserTest
+    {
+        [Test]
+        public void Parser_Test()
+        {
+
+        }
+
+    }
+}
